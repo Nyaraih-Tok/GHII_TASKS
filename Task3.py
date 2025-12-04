7f30a148a6fa48a1e110b05aca9b9b84a07a7ba3
Nyaraih-Tok <nyaraihk@gmail.com>
2025-12-04 10:38:34 +0200

    add query for adding new acc leave days

--- Task3.py
+++ Task3.py
@@ -8,9 +8,56 @@ def get_connection():
         password="",
         database="employee_leave"
     )
+
 def add_month_leave(date_str):
     conn = get_connection()
     cur = conn.cursor()
+
     d = datetime.strptime(date_str, "%Y-%m-%d")
     year = d.year
     month = d.month
+
+    cur.execute("SELECT employee_id FROM Employee")
+    employee_list = cur.fetchall()
+
+    for (emp_id,) in employee_list:
+        cur.execute("""
+            INSERT INTO LeaveEntitlement
+                (employee_id, accumulated_days, entitled_days,
+                 months_worked, period_year, period_month)
+            VALUES (%s, 1.25, 1.25, 1, %s, %s)
+        """, (emp_id, year, month))
+
+       
+        cur.execute("""
+            SELECT COALESCE(SUM(entitled_days + accumulated_days), 0)
+            FROM LeaveEntitlement
+            WHERE employee_id = %s
+        """, (emp_id,))
+        total_entitled = cur.fetchone()[0]
+
+     
+        cur.execute("""
+            SELECT COALESCE(SUM(leave_days_used), 0)
+            FROM LeaveUsage
+            WHERE employee_id = %s
+        """, (emp_id,))
+        total_used = cur.fetchone()[0]
+
+        remaining = total_entitled - total_used
+
+      
+        if month == 12:
+            remaining = remaining * 0.5
+
+       #after working on the thing this query will update the LeaveBalance model
+       # as defined by the ERD
+        cur.execute("""
+            INSERT INTO LeaveBalance
+                (employee_id, total_balance, remaining_leave_days, current_date)
+            VALUES (%s, %s, %s, %s)
+        """, (emp_id, total_entitled, remaining, date_str))
+
+    conn.commit()
+    cur.close()
+    conn.close()
