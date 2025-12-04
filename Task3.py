bc8a58037f229f21bf3f15049a747d336d2a82ac
Nyaraih-Tok <nyaraihk@gmail.com>
2025-12-04 10:23:11 +0200

    defined add leave month function

--- Task3.py
+++ Task3.py
@@ -8,3 +8,9 @@ def get_connection():
         password="",
         database="employee_leave"
     )
+def add_month_leave(date_str):
+    conn = get_connection()
+    cur = conn.cursor()
+    d = datetime.strptime(date_str, "%Y-%m-%d")
+    year = d.year
+    month = d.month
